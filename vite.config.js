6846e6e0c44509546b9da437c85e9c4dcf2abf83
Andrew Alfaro <53949770+andrewjumperdev@users.noreply.github.com>
2023-08-19 16:02:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default {
   plugins: [react()],
-  base: '/my-web-app/'
-})
+  server: {
+    port: process.env.PORT || 3030,
+  },
+}
